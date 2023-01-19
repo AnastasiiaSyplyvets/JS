e00e2ae559bac3a3ee08ab4bf004c6d737150306
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2023-01-19 20:41:24 +0100

    f
    f

---
+++ test.js
@@ -0,0 +1,36 @@
+// for (let i = 0; i<10; i+=1){
+//     console.log(i);
+// }
+// console.log("quwue");
+
+// 1. сделать вары
+// const minSalary = 500;
+// const maxSalary = 5000;
+// const employees = 4;
+// let totalSalary = 0;
+
+// // 2.перебрать работников в цикле
+// for (let i = 1; i <= employees; i += 1) {
+//     // 3.сгенерировать случайную зп
+//     const salary = Math.round(
+//         Math.random() * (maxSalary - minSalary) + minSalary,
+//         )
+//     console.log(`ЗП работника номер ${i} - ${salary}`);
+
+//     // 4.прибавить к тоталу
+//     totalSalary += salary;
+// }
+
+// console.log(`totalSalary: `, totalSalary);
+
+
+
+
+const cart = ["aa", "bb", "cc"];
+
+
+for(let i = 0; i < cart.length; i += 1) {
+
+    console.log(cart[i]);
+}
+console.log(cart);
